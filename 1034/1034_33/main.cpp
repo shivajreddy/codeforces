1c1f736cc6463580a0d4c10f90387b36899bca9c
shiva <sreddy@hey.com>
2025-07-02 19:02:29 -0400

    Contest-2123-attempt2-a,b done

---
+++ 1034/1034_33/main.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/2123/problem/C
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setupIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif // !ONLINE_JUDGE
+}
+
+int main() {
+    setupIO();
+
+    return 0;
+}
