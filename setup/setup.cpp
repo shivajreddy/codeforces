ad4fa4f3c0b73e8a178f8de72202052856e3f9d0
shiva <sreddy@hey.com>
2025-07-02 11:35:57 -0400

    .

---
+++ setup/setup.cpp
@@ -0,0 +1,19 @@
+// solution_url
+
+#include <iostream>
+using namespace std;
+
+void setupIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif // !ONLINE_JUDGE
+}
+
+int main() {
+    setupIO();
+
+    // start getting input using cin
+}
