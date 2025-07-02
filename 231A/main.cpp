7802d8c7027a03b886310acbdf2ea25bb2edfeaa
shiva <sreddy@hey.com>
2025-07-02 10:54:45 -0400

    .

--- 231A/main.cpp
+++ 231A/main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-    cout << "hello world" << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif // !ONLINE_JUDGE
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << a << " " << b << " " << c << '\n';
+    }
 }
