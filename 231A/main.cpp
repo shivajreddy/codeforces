e1baf8b903c309ba308add88048343f3a29e7073
shiva <sreddy@hey.com>
2025-07-02 12:01:20 -0400

    .

--- 231A/main.cpp
+++ 231A/main.cpp
@@ -1,18 +1,30 @@
+// https://codeforces.com/problemset/problem/231/A
+
 #include <iostream>
 using namespace std;
 
-int main() {
+void setupIO() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
 #endif // !ONLINE_JUDGE
+}
+
+int main() {
+    setupIO();
 
     int t;
     cin >> t;
-    while (t--) {
+
+    int res = 0;
+    for (int i = 0; i < t; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        cout << a << " " << b << " " << c << '\n';
+        if (a + b + c >= 2) {
+            res++;
+        }
     }
+    cout << res << "\n";
 }
