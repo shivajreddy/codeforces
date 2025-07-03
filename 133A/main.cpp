c8a8a09ab2d3b861b3564ec3dc3361e4338941a4
shiva <sreddy@hey.com>
2025-07-03 13:32:31 -0400

    .

--- 133A/main.cpp
+++ 133A/main.cpp
@@ -1,6 +1,7 @@
-// https://codeforces.com/problemset/problem/
+// https://codeforces.com/problemset/problem/133/A
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 void setupIO() {
@@ -12,6 +13,23 @@ void setupIO() {
 #endif // !ONLINE_JUDGE
 }
 
+#include <unordered_set>
+
+void solve(const string s) {
+    // instructions that produce an output
+    unordered_set<char> set = { 'H', 'Q', '9' };
+    for (char ch : s) {
+        if (set.count(ch)) {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
 int main() {
     setupIO();
+    string s;
+    cin >> s;
+    solve(s);
 }
