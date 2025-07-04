bb3f74838efbe384ff5b124ba3a46c6781b58bc4
shiva <sreddy@hey.com>
2025-07-04 12:49:00 -0400

    .

--- 1883B/main.cpp
+++ 1883B/main.cpp
@@ -11,6 +11,35 @@ void setupIO() {
 #endif
 }
 
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    // frequency counter
+    unordered_map<char, int> hm;
+    for (char c : s) {
+        if (hm.find(c) == hm.end()) hm[c] = 0;
+        hm[c] += 1;
+    }
+
+    // palidrome: all values are even, except 1
+    // remove all extra odd numbers. see if that count <= k
+
+    int count = 0;
+    for (auto [k, v] : hm) {
+        // cout << k << ":" << v << endl;
+        if (v % 2 != 0) count += 1;
+    }
+
+    (count <= k + 1) ? cout << "YES\n" : cout << "NO\n";
+}
+
 int main() {
     setupIO();
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
 }
