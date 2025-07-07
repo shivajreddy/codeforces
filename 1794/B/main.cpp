f6a0fa9c6ae63781f97c44bd719a30b7abf4e854
shiva <sreddy@hey.com>
2025-07-07 17:16:09 -0400

    .

---
+++ 1794/B/main.cpp
@@ -0,0 +1,25 @@
+// B. Not Dividing
+// https://codeforces.com/problemset/problem/1794/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void setupIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+}
+
+void solve() {
+}
+
+int main() {
+    setupIO();
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+}
