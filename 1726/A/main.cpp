b495b349409e44b2c2d8f18a941f9d0e7ac881e5
shiva <sreddy@hey.com>
2025-07-08 11:22:05 -0400

    .

---
+++ 1726/A/main.cpp
@@ -0,0 +1,25 @@
+// A. Mainak and Array
+// https://codeforces.com/problemset/problem/1726/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void setupIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+}
+
+void solve() {
+}
+
+int main() {
+    setupIO();
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+}
--- 2116/C/main.cpp
+++ 2116/C/main.cpp
@@ -13,6 +13,33 @@ void setupIO() {
 }
 
 void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    // for (int num : v) {
+    //     cout << num << " ";
+    // }
+    // cout << endl;
+
+    int res = 0;
+
+    while (true) {
+        int smallest = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            smallest = min(smallest, v[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (v[i] == smallest) continue;
+            int rem = v[i] % smallest;
+            // v[i]
+        }
+    }
+
+    cout << res << "\n";
 }
 
 int main() {
