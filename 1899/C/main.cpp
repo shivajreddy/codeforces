9c17c9569707b34ee01fe93bfb2448990795fe43
shiva <sreddy@hey.com>
2025-07-15 15:19:28 -0400

    .

--- 1899/C/main.cpp
+++ 1899/C/main.cpp
@@ -31,9 +31,55 @@ int main() {
         solve();
 }
 
+bool has_same_parity(const vector<int>& v, int idx) {
+    if (idx == 0) return false;
+    int prev = abs(v[idx - 1]);
+    int curr = abs(v[idx]);
+    // if (prev % 2 == 0 && curr % 2 == 0) return true;
+    // if (prev % 2 == 1 && curr % 2 == 1) return true;
+    // return false;
+    return (prev % 2 == curr % 2);
+}
+
 void solve() {
     int n;
     cin >> n;
     vi v(n);
     loop(i, 0, n) cin >> v[i];
+
+    int sum = v[0];
+    int res = v[0];
+
+    loop(i, 1, n) {
+        // if (sum <= 0 || has_same_parity(v, i)) {
+        if (sum <= 0 || (abs(v[i - 1]) % 2 == abs(v[i]) % 2)) {
+            sum = v[i];
+        } else {
+            sum += v[i];
+        }
+        res = max(res, sum);
+    }
+
+    cout << res << endl;
+}
+
+void solve2() {
+    int n;
+    cin >> n;
+    vi v(n);
+    loop(i, 0, n) cin >> v[i];
+
+    int sum = 0;
+    int res = v[0];
+
+    loop(i, 0, n) {
+        if (sum <= 0 || has_same_parity(v, i)) {
+            sum = v[i];
+        } else {
+            sum += v[i];
+        }
+        res = max(res, sum);
+    }
+
+    cout << res << endl;
 }
