314c5a4d0c8e99c3a936170594a7aa7c647a7650
shiva <sreddy@hey.com>
2025-07-16 13:26:50 -0400

    .

--- 1891/B/main.cpp
+++ 1891/B/main.cpp
@@ -31,24 +31,63 @@ int main() {
 }
 
 void solve() {
-    int n, q;
-    cin >> n >> q;
+    int n, m;
+    cin >> n >> m;
     vi a(n);
     loop(i, 0, n) cin >> a[i];
 
-    vi x(q);
-    loop(i, 0, q) cin >> x[i];
+    vi q(m);
+    loop(i, 0, m) cin >> q[i];
 
-    vi res = a;
+    vi max_divisors(n);
+    // go over a and check for all [1..30] the highest divisor
+    loop(i, 0, n) { // 10^5
+        int max_d = -1;
+        for (int d = 30; d >= 1; d--) { // 30
+            if (a[i] % (int)pow(2, d) == 0) {
+                max_d = d;
+                break;
+            }
+        }
+        max_divisors[i] = max_d;
+    }
+    loop(i, 0, n) cout << max_divisors[i] << " ";
+    cout << endl;
 
-    loop(i, 0, q) { // 10^5, xi=[1..30]
-        int d = pow(2, x[i]);
-        int add = pow(2, x[i] - 1);
+    vector<ll> res(n);
+    loop(i, 0, n) res[i] = a[i];
+
+    unordered_map<int, int> q_map; // { power : count}
+    loop(d, 1, 31) q_map[d] = 0;
+
+    loop(i, 0, m) { // 10^5
+        int curr_q = q[i];
+        for (int curr_d = 1; curr_d <= curr_q; curr_d++) { // 30
+            q_map[curr_d] += (int)pow(2, curr_d - 1);
+        }
+    }
+
+    vi final_adds(n, 0); // holds the final numbers that will be added
+    loop(i, 0, n) {
+        if (max_divisors[i] == -1) continue;
+        int max_divisor = max_divisors[i];
+        final_adds[i] = q_map[max_divisor];
+    }
+
+    loop(i, 0, n) { // 10^5
+        res[i] = a[i] + final_adds[i];
+    }
+
+    /*
+    loop(i, 0, m) {          // 10^5, xi=[1..30]
+        ll d = pow(2, q[i]); // d=[2..2^30]
+        ll add = pow(2, q[i] - 1);
         loop(j, 0, n) { // 10^5, ai=[1..10^9]
             if (res[j] % 2 == 1) continue;
             if (res[j] % d == 0) res[j] += add;
         }
     }
+    */
 
     loop(i, 0, n) cout << res[i] << " ";
     cout << '\n';
