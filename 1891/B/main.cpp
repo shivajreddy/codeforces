d3dbffee452508d09db0ac840a22a092be5db6a5
shiva <sreddy@hey.com>
2025-07-16 10:16:51 -0400

    .

--- 1891/B/main.cpp
+++ 1891/B/main.cpp
@@ -27,9 +27,29 @@ int main() {
 
     int tc;
     cin >> tc;
-    while (tc--)
-        solve();
+    while (tc--) solve();
 }
 
 void solve() {
+    int n, q;
+    cin >> n >> q;
+    vi a(n);
+    loop(i, 0, n) cin >> a[i];
+
+    vi x(q);
+    loop(i, 0, q) cin >> x[i];
+
+    vi res = a;
+
+    loop(i, 0, q) { // 10^5, xi=[1..30]
+        int d = pow(2, x[i]);
+        int add = pow(2, x[i] - 1);
+        loop(j, 0, n) { // 10^5, ai=[1..10^9]
+            if (res[j] % 2 == 1) continue;
+            if (res[j] % d == 0) res[j] += add;
+        }
+    }
+
+    loop(i, 0, n) cout << res[i] << " ";
+    cout << '\n';
 }
