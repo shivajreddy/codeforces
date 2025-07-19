75c9adecef3d11bda8aba338236eebcc80a71240
shiva <sreddy@hey.com>
2025-07-19 18:47:44 -0400

    .

--- 1666/D/main.cpp
+++ 1666/D/main.cpp
@@ -31,4 +31,63 @@ int main() {
 }
 
 void solve() {
+    string s, t;
+    cin >> s >> t;
+
+    int n = s.size();
+    int m = t.size();
+
+    vi freq_t(26, 0);
+    loop(i, 0, m) freq_t[t[i] - 'A']++;
+
+    for (int i = n - 1; i >= 0; i--) {
+        char c = s[i];
+        if (freq_t[c - 'A'] > 0)
+            freq_t[c - 'A']--;
+        else
+            s[i] = '.';
+    }
+
+    string final_string = "";
+    loop(i, 0, n) if (s[i] != '.') final_string += s[i];
+    final_string == t ? cout << "YES\n" : cout << "NO\n";
+}
+
+// 5:09 - 6:43
+// my solution
+void solve2() {
+    string s, t;
+    cin >> s >> t;
+
+    unordered_map<char, vi> hm;
+    loop(i, 0, s.size()) hm[s[i]].push_back(i);
+
+    unordered_map<char, int> t_map;
+    loop(i, 0, t.size()) t_map[t[i]] += 1;
+
+    for (auto [c, count] : t_map) {
+        vi& a = hm[c];
+        if (a.size() < count) {
+            cout << "NO\n";
+            return;
+        }
+        a.erase(a.begin(), a.begin() + a.size() - count);
+    }
+    // for (auto [k, v] : hm) cout << k << ":" << v << endl;
+
+    int prev_idx = -1;
+    loop(i, 0, t.size()) {
+        char c = t[i];
+
+        vi& a = hm[c];
+        int idx = a[0];
+        a.erase(a.begin());
+
+        if (idx < prev_idx) {
+            cout << "NO\n";
+            return;
+        }
+        prev_idx = idx;
+    }
+    cout << "YES\n";
 }
