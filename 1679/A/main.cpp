5477f6e47666f734293942c9cfd194fe86d6bb55
smpl <sreddy@hey.com>
2025-07-19 14:30:49 -0400

    .

--- 1679/A/main.cpp
+++ 1679/A/main.cpp
@@ -13,62 +13,111 @@ void setupIO() {
     freopen("input.txt", "r", stdin);
 #endif
 }
+/*
+x4+y6 = n
 
+OBSERVATIONS:
+
+x: min=0 max=n/4
+y: min=0 max=n/6
+
+
+while n > 0 :
+    if n % 6 == 0:
+        count6++
+        n /= 6
+    else:
+        rem++
+        n--
+if rem % 4 != 0:
+
+for min number of busses:
+                all are 6,
+                then mix with mostly 6 and other 4,
+                then no 6 only 4
+for max number of buses:
+                all are 4
+                mix of 4 and 6 - mostly 4
+                no 4 all 6
+
+*/
+
+// 1:49
 void solve() {
-    ll N;
-    cin >> N;
+    ll n;
+    cin >> n;
 
-    if (N % 4 != 0 && N % 6 != 0) {
-        cout << "-1\n";
+    ll mn = -1, mx = -1;
+
+    ll curr6, curr4;
+    // MAX
+    curr6 = n, curr4 = 0;
+    while (curr6 % 6 != 0 && curr4 % 4 != 0) {
+        curr6--;
+        curr4++;
+    }
+    if (curr6 % 6 == 0 && curr4 % 4 == 0) {
+        mx = curr6 / 6 + curr4 / 4;
     }
 
-    ll mx = N / 2;
-    ll mn = (N + 1) / 3;
-    cout << mn * 2 << " " << mx * 2 << endl;
-    return;
+    // MIN
+    curr6 = 0, curr4 = n;
+    while (curr6 % 6 != 0 && curr4 % 4 != 0) {
+        curr6++;
+        curr4--;
+    }
+    if (curr6 % 6 == 0 && curr4 % 4 == 0) {
+        mn = curr6 / 6 + curr4 / 4;
+    }
 
     /*
-    ll mn = 0, mx = 0;
+    ll count6, count4, rem;
 
-    ll n = N;
-    while (n > 0) {
-        // pick as my 6 wheelers
-        while (n % 6 == 0) {
-            mn++;
-            n /= 6;
-        }
-        while (n % 4 == 0) {
-            mn++;
-            n /= 4;
-        }
+
+    // Max
+    count6 = 0;
+    count4 = 0;
+    rem = 0;
+    if (n % 6 == 0) {
+        count6 = n / 6;
+    } else {
+        rem = n % 6;
+        count6 = (n - rem) / 6;
     }
-
-    n = N;
-    while (n > 0) {
-        // pick as my 6 wheelers
-        while (n % 4 == 0) {
-            mx++;
-            n /= 4;
-        }
-        while (n % 6 == 0) {
-            mx++;
-            n /= 6;
-        }
+    if (rem % 4 == 0) {
+        count4 = rem / 4;
+        mn = count6 + count4;
     }
-    cout << mn << " " << mx << endl;
+    // 548 : 540 8
+    // mn:92 mx:137
 
-
-    ll a, b, res = -1;
-    for (int a = 0; a < n / 4; a++) {
-        for (int b = 0; b < n / 6; b++) {
-            cout << "a:" << a << " b:" << b << endl;
-            res = 4 * a + 6 * b;
-            if (res == n) break;
-        }
+    // Min
+    count6 = 0;
+    count4 = 0;
+    rem = 0;
+    if (n % 4 == 0) {
+        count4 = n / 4;
+    } else {
+        rem = n % 4;
+        count4 = (n - rem) / 4;
     }
-
-    cout << res << "\n";
+    if (rem % 6 == 0) {
+        count6 = rem / 6;
+        mx = count6 + count4;
+    }
+    cout << "mn:" << mn << " mx:" << mx << endl;
     */
+
+    // Couldnt form x6+y4=n
+    if (mn == -1 && mx == -1) {
+        cout << "-1\n";
+    } else if (mn != -1 && mx == -1) {
+        cout << mn << " " << mn << endl;
+    } else if (mx != -1 && mn == -1) {
+        cout << mx << " " << mx << endl;
+    } else {
+        cout << mn << " " << mx << endl;
+    }
 }
 
 int main() {
@@ -76,6 +125,5 @@ int main() {
 
     int tc;
     cin >> tc;
-    while (tc--)
-        solve();
+    while (tc--) solve();
 }
