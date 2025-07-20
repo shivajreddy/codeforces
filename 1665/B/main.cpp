01e1b4f805ad6691e7f4b36982c1b4dc6392f4a8
shiva <sreddy@hey.com>
2025-07-19 20:39:49 -0400

    .

--- 1665/B/main.cpp
+++ 1665/B/main.cpp
@@ -31,4 +31,36 @@ int main() {
 }
 
 void solve() {
+    int n;
+    cin >> n;
+
+    int hi = 0;
+    int rem = 0;
+    // using unordered_map instead of map is causing TLE
+    map<int, int> hm; // frequency map
+
+    loop(i, 0, n) {
+        int num;
+        cin >> num;
+        hm[num]++;
+        hi = max(hi, hm[num]);
+    }
+
+    rem = n - hi;
+    if (rem == 0) {
+        cout << "0\n";
+        return;
+    }
+
+    int res = 0;
+
+    while (rem > 0) {
+        int taken = rem - hi < 0 ? rem : hi;
+        res += (1 + taken); // 1 for clone
+
+        rem -= hi;
+        hi *= 2;
+    }
+
+    cout << res << endl;
 }
