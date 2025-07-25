8cc5bf9c9c4264e09377e3be5fea27464eaac84b
shiva <sreddy@hey.com>
2025-07-25 15:30:37 -0400

    .

--- 1374/B/main.cpp
+++ 1374/B/main.cpp
@@ -15,6 +15,25 @@ typedef vector<int> vi;
 // }
 
 void solve() {
+    int n;
+    cin >> n;
+
+    int count3 = 0;
+    while (n > 0 && n % 3 == 0) {
+        count3++;
+        n /= 3;
+    }
+
+    int count2 = 0;
+    while (n > 0 && n % 2 == 0) {
+        count2++;
+        n /= 2;
+    }
+    if (n > 1 || count2 > count3) {
+        cout << -1 << endl;
+    } else {
+        cout << count3 + (count3 - count2) << endl;
+    }
 }
 
 int main() {
