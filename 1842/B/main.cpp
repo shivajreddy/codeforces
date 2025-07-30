3bd65930dc1763ccde60c98e6d82446c1d8a688f
shiva <sreddy@hey.com>
2025-07-30 11:27:50 -0400

    .

---
+++ 1842/B/main.cpp
@@ -0,0 +1,39 @@
+// B. Tenzing and Books
+// https://codeforces.com/problemset/problem/1842/B
+
+// { Imports, TypeNames, Macros
+#include <bits/stdc++.h>
+using namespace std;
+
+// shorter type names
+typedef long long ll;
+typedef vector<int> vi;
+
+// Macros
+#define PB push_back
+#define loop(i, a, b) for (int i = a; i < b; i++)
+// }
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+
+    vi a(n);
+    loop(i, 0, n) cin >> a[i];
+    vi b(n);
+    loop(i, 0, n) cin >> b[i];
+    vi c(n);
+    loop(i, 0, n) cin >> c[i];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--) solve();
+}
