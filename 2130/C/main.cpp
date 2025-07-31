d18917bdaa2a5e908c72263055e8c6810cd20b78
shiva <sreddy@hey.com>
2025-07-31 13:56:41 -0400

    .

--- 2130/C/main.cpp
+++ 2130/C/main.cpp
@@ -15,6 +15,32 @@ typedef vector<int> vi;
 // }
 
 void solve() {
+    int n;
+    cin >> n;
+
+    vector<vi> edges(n, vector<int>(3));
+    loop(i, 0, n) {
+        cin >> edges[i][0] >> edges[i][1];
+        edges[i][2] = i; // original index
+    }
+
+    // maximize f(s), minimize g(s)
+    // for triangle of points a,b,c where a-c is straight,
+    // then a-b + b-c >= a-c, also eliminating a-c will reduce g(s)
+    // line sweep maybe
+    sort(edges.begin(), edges.end());
+    vi idx_but_sorted = { 0 };
+
+    for (int i = 1; i < edges.size(); i++) {
+        int prev_end = edges[idx_but_sorted.back()][1];
+        if (prev_end <= edges[i][0]) {
+            idx_but_sorted.push_back(i);
+        }
+    }
+
+    cout << idx_but_sorted.size() << endl;
+    for (int idx : idx_but_sorted) cout << edges[idx][2] + 1 << " ";
+    cout << endl;
 }
 
 int main() {
