d0d47717d92bdb169c07d48f9d9b84c4ca3f62f5
smpl <sreddy@hey.com>
2025-08-01 23:01:00 -0400

    .

--- 1914/D/main.cpp
+++ 1914/D/main.cpp
@@ -3,6 +3,7 @@
 
 // { Imports, TypeNames, Macros
 #include <bits/stdc++.h>
+#include <functional>
 using namespace std;
 
 // shorter type names
@@ -14,6 +15,19 @@ typedef vector<int> vi;
 #define loop(i, a, b) for (int i = a; i < b; i++)
 // }
 
+vi find_max(vi& arr) {
+    int n = arr.size();
+    vector<pair<int, int>> tmp(n);
+    loop(i, 0, n) {
+        tmp[i].first = arr[i];
+        tmp[i].second = i;
+    }
+    sort(tmp.begin(), tmp.end(), greater<pair<int, int>>());
+    vi res(3);
+    loop(i, 0, 3) res[i] = tmp[i].second;
+    return res;
+}
+
 void solve() {
     int n;
     cin >> n;
@@ -24,6 +38,18 @@ void solve() {
     loop(i, 0, n) cin >> b[i];
     vi c(n);
     loop(i, 0, n) cin >> c[i];
+
+    vi max_a = find_max(a);
+    vi max_b = find_max(b);
+    vi max_c = find_max(c);
+
+    int res = 0;
+    loop(i, 0, 3) loop(j, 0, 3) loop(k, 0, 3) {
+        int mxa = max_a[i], mxb = max_b[j], mxc = max_c[k];
+        if ((mxa == mxb) || (mxb == mxc) || (mxc == mxa)) continue;
+        res = max(res, a[mxa] + b[mxb] + c[mxc]);
+    }
+    cout << res << endl;
 }
 
 int main() {
