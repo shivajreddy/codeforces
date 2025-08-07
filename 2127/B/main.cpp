160a2ad4ba61893f8c2ca0f90c84aa9a67fd1583
shiva <sreddy@hey.com>
2025-08-07 11:10:45 -0400

    .

---
+++ 2127/B/main.cpp
@@ -0,0 +1,33 @@
+// B. Hamiiid, Haaamid... Hamid?
+// https://codeforces.com/contest/2127/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// shorter type names
+typedef long long ll;
+typedef vector<int> vi;
+
+// Macros
+#define PB push_back
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+
+    vi a(n);
+    loop(i, 0, n) cin >> a[i];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--) solve();
+}
